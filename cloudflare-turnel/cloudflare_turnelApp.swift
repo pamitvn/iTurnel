b6037084ddbc0ab02e17ea1c9d50572d35e0fc94
PAM <anpm@trustshop.io>
2025-12-29 10:58:40 +0700

    Initial Commit

---
+++ cloudflare-turnel/cloudflare_turnelApp.swift
@@ -0,0 +1,17 @@
+//
+//  cloudflare_turnelApp.swift
+//  cloudflare-turnel
+//
+//  Created by TrustShop Develop on 29/12/25.
+//
+
+import SwiftUI
+
+@main
+struct cloudflare_turnelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
